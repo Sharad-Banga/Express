5a6f7af7db412639f703162bf216b5d45dacb014
Sharad Banga <sharadiit03@gmail.com>
2025-01-01 19:22:54 +0530

    updation file

--- Auth/index1.js
+++ Auth/index1.js
@@ -3,3 +3,63 @@ const app = express();
 
 const jwt = require("jsonwebtoken");
 
+app.use(express.json());
+
+const JWT_SECRET = "sharad123";
+
+const users = [];
+
+
+//adding signup credentials into the array
+app.post("/signup",(req,res)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+  users.push({
+    username : username,
+    password : password
+  })
+
+  //check if user is present or not
+  res.json({
+    message : "You are signed in"
+  })
+
+})
+
+
+
+app.post("/signin",(req,res)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const foundUser = users.find((u)=>{
+    if(u.username == username && u.password==password){
+        return true;
+    }
+    else{
+      return false;
+    }
+  })
+
+  if(!foundUser){
+    res.json({
+      message:"credentials incorrect"
+    })
+  }
+  else{
+    
+    const token = jwt.sign(foundUser.username ,JWT_SECRET);
+    res.json({
+      token : token
+    })
+  }
+
+
+})
+
+app.get("/me",(req,res)=>{
+
+})
+
+
+app.listen(3000);
